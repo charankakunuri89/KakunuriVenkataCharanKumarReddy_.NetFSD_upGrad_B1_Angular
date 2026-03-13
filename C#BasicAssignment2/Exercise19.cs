b1f66c9023ebc5b605546d73f33b9a883d575e0d
charankakunuri89 <charankakunuri89@gmail.com>
2026-03-13 15:44:59 +0530

    Add files via upload

---
+++ C#BasicAssignment2/Exercise19.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Assignment2
+{
+    internal class Exercise19
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check whether the given word is palindrome or not");
+            string input = Console.ReadLine();
+            string rev = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                rev = input[i] + rev;
+            }
+            if (input == rev)
+            {
+                Console.WriteLine("Given word is a Palindrome");
+
+            }
+            else
+            {
+                Console.WriteLine("Not a Palindrome");
+
+            }
+        }
+
+    }
+}
---
+++ C#BasicAssignment2/Exercise2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Assignment2
+{
+    internal class Exercise2
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string input = args[0];
+                Console.WriteLine($"Hi! {input} ");
+                Console.WriteLine($"Welcome to the world of C#");
+            }
+            else
+            {
+                Console.WriteLine("Please provide your name as a command line argument.");
+            }
+        }
+    }
+}
