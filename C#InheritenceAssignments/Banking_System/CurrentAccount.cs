25c3920aa66ac6dfd6350badfed03429600278c0
charankakunuri89 <charankakunuri89@gmail.com>
2026-03-17 12:34:28 +0530

    Add files via upload

---
+++ C#InheritenceAssignments/Banking_System/CurrentAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance_Assignment.Banking_System
+{
+    internal class CurrentAccount : Account
+    {
+        public new void CalculateInterest()
+        {
+            Console.WriteLine("Current Account Interest");
+        }
+    }
+}
