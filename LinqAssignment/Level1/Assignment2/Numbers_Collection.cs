b0102306bb403c9b54444df8dee1ba929980b6ba
charankakunuri89 <charankakunuri89@gmail.com>
2026-03-24 21:02:47 +0530

    Add files via upload

---
+++ LinqAssignment/Level1/Assignment2/Numbers_Collection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinqAssignment.Level1.Assignment2
+{
+    internal class Numbers_Collection
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 5, 10, 15, 20, 25, 30 };
+
+            //1.Get even numbers
+            var evenNumbers = numbers.Where(n => n % 2 == 0).ToList();
+
+            foreach (var n in evenNumbers)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine();
+            //2.Get numbers greater than 15
+            var numbersGreaterThan15 = numbers.Where(n => n > 15).ToList();
+
+            foreach(var n in numbersGreaterThan15)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine();
+            //3.find square of each number
+            var squareOfEachNumber = numbers.Select(n =>  n * n).ToList();
+
+            foreach(var n in squareOfEachNumber)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine();
+            //4.Count no.of numbers divisible by 5
+            var countOfNumbersDivisibleBy5 = numbers.Where(n => n % 5 == 0).Count();
+            Console.WriteLine(countOfNumbersDivisibleBy5);
+        }
+    }
+}
